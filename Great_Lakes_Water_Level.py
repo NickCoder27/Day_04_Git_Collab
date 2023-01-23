1d61564500021c5fb19fcc317bf294aadd080993
LucasReath <reathluc@msu.edu>
2023-01-23 11:47:52 -0500

    Luke's changes

--- Great_Lakes_Water_Level.py
+++ Great_Lakes_Water_Level.py
@@ -26,7 +26,7 @@ import matplotlib.pyplot as plt
 # Using the Michigan/Huron Dataset, plot the Water Level, the second 
 # column, as a function of time years
 mich_huron = pd.read_csv('eri.csv')
-#mich_huron is weird
+mich_huron
 
 
 # PART 2
